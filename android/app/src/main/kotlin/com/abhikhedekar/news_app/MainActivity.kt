3448f5739fb62fe4d1f6c4e9cf9c8f8d38555c72
Abhishek Khedekar <abhishek@Abhisheks-MacBook-Pro.local>
2021-08-15 10:45:31 +0530

    build branch

--- android/app/src/main/kotlin/com/abhikhedekar/news_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.abhikhedekar.news_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'SizeConfig.dart';
-
-const String apiKey = '03bd95954ad4479daccdc94c64bf1134';
-
-TextStyle kTabBarTextStyle = TextStyle(
-    fontSize: 15, fontFamily: 'OpenSans', fontWeight: FontWeight.bold);
